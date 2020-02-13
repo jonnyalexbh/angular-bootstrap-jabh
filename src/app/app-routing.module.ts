8cea6f473e57fb1d2214d9deeeb3576ec0c45755
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-02-13 10:47:53 -0500

    add routes and create home and album components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './screens/home/home.component';
+import { AlbumsComponent } from './screens/albums/albums.component';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'albums',
+    component: AlbumsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './screens/home/home.component';
+import { AlbumsComponent } from './screens/albums/albums.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    HomeComponent,
+    AlbumsComponent
   ],
   imports: [
     BrowserModule,
