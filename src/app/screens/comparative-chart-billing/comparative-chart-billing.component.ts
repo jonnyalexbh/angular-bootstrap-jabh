64ffc782fed43db99798d63de6f27ce7eb1264c0
jonnyalexbh <jonnyalex.bh@gmail.com>
2020-03-18 14:57:01 -0500

    comparative chart billing with mock

--- src/app/screens/comparative-chart-billing/comparative-chart-billing.component.ts
+++ src/app/screens/comparative-chart-billing/comparative-chart-billing.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import { dataMonth } from './data-month';
+import { dataQuarter } from './data-quarter';
 
 @Component({
   selector: 'app-comparative-chart-billing',
@@ -6,10 +9,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./comparative-chart-billing.component.scss']
 })
 export class ComparativeChartBillingComponent implements OnInit {
+  @ViewChild('charts') public chartEl: ElementRef;
+
+  dataService: any;
+  periods: Array<string> = ['MES', 'TRIMESTRE', 'SEMESTRE', 'AÑO'];
+  indexLbl: number;
+
+  get getGroupLabels() {
+    return this.dataService.me.group_labels;
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.getBillingComparison();
+  }
+
+  getBillingComparison = (period = 0) => {
+    this.dataService = period === 0 ? dataMonth : dataQuarter;
+    this.indexLbl = this.dataService.me.current_period;
+    this.setChart();
+  }
+
+  changePeriods(period) {
+    this.getBillingComparison(period);
+  }
+
+  selectPeriod(index) {
+    this.indexLbl = index;
+    this.setChart();
+  }
+
+  setChart = () => {
+    const options = {
+      chart: {
+        type: 'column',
+      },
+      title: {
+        text: 'Facturación año'
+      },
+      xAxis: {
+        categories: this.dataService.me.billing_data[this.indexLbl].labels
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: ''
+        }
+      },
+      legend: {
+        reversed: true
+      },
+      plotOptions: {
+        series: {
+          stacking: 'normal',
+          cursor: 'pointer',
+        },
+      },
+      series: [{
+        data: this.dataService.me.billing_data[this.indexLbl].values,
+        color: 'rgba(0, 118, 255,1)',
+        showInLegend: false,
+      }]
+    };
+    Highcharts.chart(this.chartEl.nativeElement, options);
   }
 
 }
---
+++ src/app/screens/comparative-chart-billing/data-quarter.ts
@@ -0,0 +1,77 @@
+export const dataQuarter = {
+  me: {
+    current_period: 0,
+    last_load_transactions: 'a 29 de febrero',
+    billing_range: '1 ene 2020 - 31 dic 2020',
+    current_billing: 142603150,
+    goal: 0,
+    labels: '',
+    group_labels: [
+      'TRIMESTRE 1',
+      'TRIMESTRE 2',
+      'TRIMESTRE 3',
+      'TRIMESTRE 4'
+    ],
+    years: [
+      2018,
+      2019,
+      2020
+    ],
+    billing_data: [
+      {
+        values: [
+          75649470,
+          66953680,
+          0
+        ],
+        labels: [
+          'Enero',
+          'Febrero',
+          'Marzo'
+        ]
+      },
+      {
+        values: [
+          50649470,
+          45649470,
+          30649470
+        ],
+        labels: [
+          'Abril',
+          'Mayo',
+          'Junio'
+        ]
+      },
+      {
+        values: [
+          0,
+          0,
+          0
+        ],
+        labels: [
+          'Julio',
+          'Agosto',
+          'Septiembre'
+        ]
+      },
+      {
+        values: [
+          0,
+          0,
+          0
+        ],
+        labels: [
+          'Octubre',
+          'Noviembre',
+          'Diciembre'
+        ]
+      }
+    ]
+  },
+  metadata: {
+    id: 3,
+    unavailableMessage: null,
+    tags: [],
+    feedback: false
+  }
+}
